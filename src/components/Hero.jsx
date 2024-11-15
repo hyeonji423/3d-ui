5c57219dc5ca84a1f06b8913df028df02e8f47b7
hyeonji423 <hyeon423423@naver.com>
2024-11-15 18:08:07 +0900

    1115

---
+++ src/components/Hero.jsx
@@ -0,0 +1,56 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useEffect, useState } from 'react'
+import { heroVideo, smallHeroVideo } from '../utils'
+
+const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 768 ? smallHeroVideo : heroVideo
+  )
+
+  const handleVideoSrc = () =>{
+    if(window.innerWidth < 768){
+      setVideoSrc(smallHeroVideo)
+    }
+    else{
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize', handleVideoSrc)
+
+    // clean up
+    return () =>{
+      window.removeEventListener('resize', handleVideoSrc)
+    }
+  }, [])
+
+  
+  useGSAP(()=>{
+    gsap.to('#hero', {opacity:1, delay:2, duration:2})
+    gsap.to('#bt', {opacity:1, y:-20, delay:2, duration:1, ease:'expo.out'})
+  }, [])
+
+
+  return (
+    <section className='w-full nav-height bg-black relative flex flex-center'>
+      <div className='h-5/6 flex-center w-full flex-col'>
+        <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+
+        <div className='w-9/12'>
+          <video autoPlay muted key={videoSrc} className='pointer-events-none'>
+            <source src={videoSrc} type='video/mp4' />
+          </video>
+        </div>
+
+        <div id='bt' className='flex flex-col items-center translate-y-20 opacity-0'>
+          <a href="#highlights" className='btn'>Buy</a>
+          <p className='font-normal text-xl'>From $199/month or $999</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
